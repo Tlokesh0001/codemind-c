52f67f6c28d58b3bfe5c5ddac81f2aec8ba51d7c
Lokesh Kumar Talluri <106659982+Tlokesh0001@users.noreply.github.com>
2022-10-09 17:08:08 +0530

    #include<bits/stdc++.h> #include<iomanip> using namespace std;
    int main() {
        int r;
        cin >> r;
        double ans = (3.14*r*r);
        cout<<setprecision(2)<<fixed;
        cout <<ans << endl;
    }

---
+++ Area_of_a_circle.c
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    int r;
+    cin >> r;
+    double ans = (3.14*r*r);
+    cout<<setprecision(2)<<fixed;
+    cout <<ans << endl;
+}
