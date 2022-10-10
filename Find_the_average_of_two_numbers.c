bb5aa7a76bcca43bbd6f86f4923da9b6f7f9d5f2
Lokesh Kumar Talluri <106659982+Tlokesh0001@users.noreply.github.com>
2022-10-10 14:27:36 +0530

    #include<bits/stdc++.h> #include<iomanip> using namespace std;
    int main() {
        double n ,m;
        cin >> n >> m;
        double ans = (n+m)/(double)(2);
        cout << setprecision(4) << fixed;
        cout << ans << "
    ";
    }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    double n ,m;
+    cin >> n >> m;
+    double ans = (n+m)/(double)(2);
+    cout << setprecision(4) << fixed;
+    cout << ans << "
+";
+}
